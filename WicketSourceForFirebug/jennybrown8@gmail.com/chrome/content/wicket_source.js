e897662241431ffb6bba665046a5fa930453be66
Jenny Brown <jennybrown8@gmail.com>
2011-10-23 14:34:28 -0500

    Major overhaul for name scope and OO issues. Basically works, except for multiple tabs.

--- WicketSourceForFirebug/jennybrown8@gmail.com/chrome/content/wicket_source.js
+++ WicketSourceForFirebug/jennybrown8@gmail.com/chrome/content/wicket_source.js
@@ -1,5 +1,33 @@
 FBL.ns(function() { with (FBL) {
 
+const Cc = Components.classes;
+const Ci = Components.interfaces;	
+
+var uniqueId = 0;
+function getNextUniqueId() {
+	uniqueId++;
+	return uniqueId;
+}
+
+var prefWatcher = {
+		prefManager : null,
+		
+		startup: function() {
+			this.prefManager = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch);
+			this.prefManager.getBranch("extensions.firebug.wicketsource").QueryInterface(Components.interfaces.nsIPrefBranch2);  
+			this.prefManager.addObserver("", this, false);  
+		},
+
+		observe: function(subject, topic, data) {
+		    if (topic != "nsPref:changed")
+		    {
+		      return;
+		    }
+		    if (FBTrace.DBG_PANELS) FBTrace.sysout("wicketsource pref changed. ", data);
+		}
+};
+prefWatcher.startup();
+
 /**
  * WicketSource displays the wicketsource='foo.bar:HelloWorld.java:62' html attribute, and
  * produces a link that can be clicked to open the Java file in an Eclipse editor.  It does
@@ -9,200 +37,190 @@ FBL.ns(function() { with (FBL) {
  * User preferences are available to configure the server, port, and timeout, but not the full uri.
  * 
  */
-
-function WicketSourcePanel() {}
+function WicketSourcePanel() {
+}
 
 WicketSourcePanel.prototype = extend(Firebug.Panel,
 {
-    name: "wicket_source",
+    name: "wicket_source" + getNextUniqueId(),
     title: "WicketSource",
     parentPanel: "html",
     order: 4,
     enableA11y: true,
     deriveA11yFrom: "console",
-	xmlHttp : null,
 	wicketPanel : null,
-	selectedWicketSource : null,
-	selectedWicketId : null,
-	requestTimer : null,
-    
 
-	makeUrl : function()
-	{
-		var url = "http://" + prefWatcher.prefManager.getCharPref("extensions.firebug.wicketsource.server") 
-		+ ":" + prefWatcher.prefManager.getIntPref("extensions.firebug.wicketsource.port") 
-		+ "/open?src=" + encodeURIComponent(wicketPanel.selectedWicketSource);
-		return url;
-	},
-	displayWicket : function() 
-	{
-	    var pieces = wicketPanel.selectedWicketSource.split(":");
-	    var packageName = pieces[0];
-	    var sourceFile = pieces[1];
-	    var lineNumber = pieces[2];
-	    var root = wicketPanel.getWicketSourceDomplateRoot();
-	    root.wicketElement.replace({
-			packageName: packageName, 
-	    	sourceFile: sourceFile, 
-	    	lineNumber: lineNumber, 
-	    	wicketId:wicketPanel.selectedWicketId, 
-	    	wicketsource:wicketPanel.selectedWicketSource,
-	    	eclipseResult:wicketPanel.eclipseResult,
-	    	server: prefWatcher.prefManager.getCharPref("extensions.firebug.wicketsource.server"),
-	    	port: prefWatcher.prefManager.getIntPref("extensions.firebug.wicketsource.port"),
-	    	timeout: prefWatcher.prefManager.getIntPref("extensions.firebug.wicketsource.timeout")
-	    }, wicketPanel.panelNode, root);
-	},	
-	displayWicketEmpty : function()
-	{
-    	var root = wicketPanel.getWicketSourceDomplateRoot();
-    	root.chooseElement.replace({}, wicketPanel.panelNode, root);
-	},
-	
-	/**
-	 * Success/failure message from the most recent http connection
-	 */
-	eclipseResult : "",
-	getWicketSourceDomplateRoot : function() {
-		return domplate (
-			{
-				chooseElement:
-					DIV({style: "padding: 4px;"}, P("Inspect an element with a 'wicketsource' attribute to see details.")),
-				wicketElement:
-					DIV({style: "padding: 4px;"},
-						TABLE({style: "margin-right: 10px;", onclick:"$handleClick"},
-								TR(TD("Wicket")),
-								TR(TD({style: "color: gray;"},"wicket:id "),TD(" "),TD("$wicketId")),
-								TR(TD({style: "color: gray;"},"package "),TD(" "),TD("$packageName")),
-								TR(TD({style: "color: gray;"},"source "),TD(" "),TD(A("$sourceFile:$lineNumber")))
-						),
-						P(),
-						TABLE({style: "margin-right: 10px;"},
-								TR(TD("Eclipse")),
-								TR(TD({style: "color: gray;"},"Request"),TD(" "),TD("$eclipseResult")),
-								TR(TD({style: "color: gray;"},"PluginServer"),TD(" "),TD("$server")),
-								TR(TD({style: "color: gray;"},"PluginPort"),TD(" "),TD("$port")),
-								TR(TD({style: "color: gray;"},"Timeout ms"),TD(" "),TD({style: "color: gray;"},"$timeout"))
-						)
-					),
-				handleClick: function(event)
-				{
-					if (event.target.tagName != 'A') { return; }
-					if (FBTrace.DBG_PANELS) FBTrace.sysout("wicketsource, click handler event ", event);
-					
-					wicketPanel.eclipseResult = "...requesting...";
-					wicketPanel.displayWicket();
-					
-					var url = wicketPanel.makeUrl();
-					if (FBTrace.DBG_PANELS) FBTrace.sysout("wicketsource, click handler launching url ", url);
-					
-					wicketPanel.requestTimer = setTimeout(function() {
-			            if (FBTrace.DBG_PANELS) FBTrace.sysout("wicketsource timed out on url, aborting.");
-			            wicketPanel.eclipseResult = "ERROR, Timed out at " + prefWatcher.prefManager.getIntPref("extensions.firebug.wicketsource.timeout") + " ms.";
-			            wicketPanel.displayWicket();
-			            wicketPanel.xmlHttp.abort();
-					  }, prefWatcher.prefManager.getIntPref("extensions.firebug.wicketsource.timeout"));
-					
-					wicketPanel.xmlHttp.open("GET",url,true);
-					wicketPanel.xmlHttp.send();
-				}
-			});	
+	onObjectSelected : function(object, panel) {
+		this.context.tabData.onObjectSelected(object, panel);
 	},
     initialize: function() {
         Firebug.Panel.initialize.apply(this, arguments);
+//        var wm = Components.classes["@mozilla.org/appshell/window-mediator;1"].getService(Components.interfaces.nsIWindowMediator);
+//        var window = wm.getMostRecentWindow("navigator:browser");
+        this.context.tabData = new TabData();
+        this.context.tabData.initialize(this);
         wicketPanel = this;
-        wicketPanel.xmlHttp = new XMLHttpRequest();
-        wicketPanel.xmlHttp.onreadystatechange = function() {
-	 		if (wicketPanel.xmlHttp.readyState == 4) {
-	 			clearTimeout(wicketPanel.requestTimer);
-	 	        if (FBTrace.DBG_PANELS) FBTrace.sysout("wicketsource readyState, clearing timer. Status = " + wicketPanel.xmlHttp.status, wicketPanel.xmlHttp);
-	 			if (wicketPanel.xmlHttp.status == 200) {
-	 				wicketPanel.eclipseResult = "OK";
-	 			} else {
-	 				wicketPanel.eclipseResult = "Error connecting. Is your WicketSource Eclipse plugin configured and running?";
-	 			}
-	 			wicketPanel.displayWicket();
-	 		}
-	 	};
-	 	wicketPanel.displayWicketEmpty();
+        if (FBTrace.DBG_PANELS) FBTrace.sysout("wicketsource, -- Panel Initialize -- called ", this);
     }	
-	
 });
 
-function populatePrefPane() 
-{
-	if (FBTrace.DBG_PANELS) FBTrace.sysout("wicketsource populate pref pane");
+
+//function populatePrefPane() 
+//{
+//	if (FBTrace.DBG_PANELS) FBTrace.sysout("wicketsource populate pref pane");
+//}
+
+
+
+function TabData() {
 }
 
-var prefWatcher = {
-	prefManager : null,
-	
-	startup: function() {
-		this.prefManager = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch);
-		this.prefManager.getBranch("extensions.firebug.wicketsource").QueryInterface(Components.interfaces.nsIPrefBranch2);  
-		this.prefManager.addObserver("", this, false);  
-	},
 
-	observe: function(subject, topic, data) {
-	    if (topic != "nsPref:changed")
-	    {
-	      return;
-	    }
-	    if (FBTrace.DBG_PANELS) FBTrace.sysout("wicketsource pref changed. ", data);
-	},
+TabData.prototype = {
+		myId : getNextUniqueId(),
+		wicketPanel : null,
+		xmlHttp : null,
+		selectedWicketSource : null,
+		selectedWicketId : null,
+		requestTimer : null,
+		eclipseResult : "",
 		
+		makeUrl : function()
+		{
+			var url = "http://" + prefWatcher.prefManager.getCharPref("extensions.firebug.wicketsource.server") 
+			+ ":" + prefWatcher.prefManager.getIntPref("extensions.firebug.wicketsource.port") 
+			+ "/open?src=" + encodeURIComponent(this.selectedWicketSource);
+			return url;
+		},
+		displayWicket : function() 
+		{
+		    var pieces = this.selectedWicketSource.split(":");
+		    var packageName = pieces[0];
+		    var sourceFile = pieces[1];
+		    var lineNumber = pieces[2];
+		    var root = this.getWicketSourceDomplateRoot();
+		    root.wicketElement.replace({
+				packageName: packageName, 
+		    	sourceFile: sourceFile, 
+		    	lineNumber: lineNumber, 
+		    	wicketId:this.selectedWicketId, 
+		    	wicketsource:this.selectedWicketSource,
+		    	eclipseResult:this.eclipseResult,
+		    	tabData:this,
+		    	server: prefWatcher.prefManager.getCharPref("extensions.firebug.wicketsource.server"),
+		    	port: prefWatcher.prefManager.getIntPref("extensions.firebug.wicketsource.port"),
+		    	timeout: prefWatcher.prefManager.getIntPref("extensions.firebug.wicketsource.timeout")
+		    }, this.wicketPanel.panelNode, this);
+		},	
+		displayWicketEmpty : function()
+		{
+	    	var root = this.getWicketSourceDomplateRoot();
+	    	root.chooseElement.replace({}, this.wicketPanel.panelNode, this);
+		},
+		
+		/**
+		 * Success/failure message from the most recent http connection
+		 */
+		getWicketSourceDomplateRoot : function(tabData) {
+			return domplate ({
+					chooseElement:
+						DIV({style: "padding: 4px;"}, P("Inspect an element with a 'wicketsource' attribute to see details.")),
+					wicketElement:
+						DIV({style: "padding: 4px;"},
+							TABLE({style: "margin-right: 10px;", onclick:"$handleLinkClick"},
+									TR(TD("Wicket")),
+									TR(TD({style: "color: gray;"},"wicket:id "),TD(" "),TD("$wicketId")),
+									TR(TD({style: "color: gray;"},"package "),TD(" "),TD("$packageName")),
+									TR(TD({style: "color: gray;"},"source "),TD(" "),TD(A("$sourceFile:$lineNumber")))
+							),
+							P(),
+							TABLE({style: "margin-right: 10px;"},
+									TR(TD("Eclipse")),
+									TR(TD({style: "color: gray;"},"Request"),TD(" "),TD("$eclipseResult")),
+									TR(TD({style: "color: gray;"},"PluginServer"),TD(" "),TD("$server")),
+									TR(TD({style: "color: gray;"},"PluginPort"),TD(" "),TD("$port")),
+									TR(TD({style: "color: gray;"},"Timeout ms"),TD(" "),TD({style: "color: gray;"},"$timeout"))
+							)
+						)
+				});	
+		},
+		handleLinkClick : function(event) {
+			if (event.target.tagName != 'A') { return; }
+			this.eclipseResult = "...requesting...";
+			this.displayWicket();
+			var url = this.makeUrl();
+			var myTabDataRef = this;
+			this.requestTimer = setTimeout(function() {
+	            myTabDataRef.eclipseResult = "ERROR, Timed out at " + prefWatcher.prefManager.getIntPref("extensions.firebug.wicketsource.timeout") + " ms.";
+	            myTabDataRef.displayWicket();
+	            myTabDataRef.xmlHttp.abort();
+			  }, prefWatcher.prefManager.getIntPref("extensions.firebug.wicketsource.timeout"));
+			this.xmlHttp.open("GET",url,true);
+			this.xmlHttp.send();			
+		},
+		onObjectSelected : function(object, panel) { 
+	        this.eclipseResult = "";
+	        if (object.hasAttribute("wicketsource")) {
+	        	this.selectedWicketSource = object.getAttribute("wicketsource");
+	            this.selectedWicketId = object.getAttribute("wicket:id"); 
+	            this.displayWicket();
+	        } else {
+	        	this.selectedWicketSource = null;
+	        	this.selectedWicketId = null;
+	            this.displayWicketEmpty();
+	        };
+		},
+		onReady : function() {
+	 		if (this.readyState == 4) {
+	 			clearTimeout(this.tabDataRef.requestTimer);
+	 			if (this.status == 200) {
+	 				this.tabDataRef.eclipseResult = "OK";
+	 			} else {
+	 				this.tabDataRef.eclipseResult = "Error connecting. Is your WicketSource Eclipse plugin configured and running?";
+	 			}
+	 			this.tabDataRef.displayWicket();
+	 		}
+		},
+		initialize : function(wicketPane) {
+			this.wicketPanel = wicketPane;
+			if (FBTrace.DBG_PANELS) FBTrace.sysout("wicketsource, tabData ----- INITIALIZE ----- called", this);
+			var myTabDataRef = this;
+			this.xmlHttp = new XMLHttpRequest();
+			this.xmlHttp.tabDataRef = this;
+			this.xmlHttp.onreadystatechange = this.onReady;
+		 	this.displayWicketEmpty();
+		}
 };
-prefWatcher.startup();
-
-
-
 
 Firebug.WicketSourceModel = extend(Firebug.Module,
 {
+	isWicketSourcePanel : function(panel) {
+		return (panel && panel.name.substring(0,13) == "wicket_source");	
+	},
     showPanel: function(browser, panel) {
-        var isWicketSourcePanel = panel && panel.name == "wicket_source";
+        var isWicketSourcePanel = this.isWicketSourcePanel(panel);
         var wicket_sourceButtons = browser.chrome.$("fbwicket_sourceButtons");
         collapse(wicket_sourceButtons, !isWicketSourcePanel);
+        if (FBTrace.DBG_PANELS) FBTrace.sysout("wicketsource, showPanel called for panel ", panel);
 
         if (!isWicketSourcePanel) return;
-        var doc = panel.document;
         
         if (!this.initialized) {
-            var t = doc.createTextNode('I am the Wicket Source extension. Hello World.');
-            panel.panelNode.appendChild(t);
+            panel.panelNode.appendChild(panel.document.createTextNode('One-time initialization of the Wicket Source extension.'));
             this.initialized = true;
         }
-
     },
-    
     onObjectSelected: function(object, panel)
     {
-        var isWicketSourcePanel = panel && panel.name == "wicket_source";
+    	var isWicketSourcePanel = this.isWicketSourcePanel(panel);
         if (!isWicketSourcePanel) return;
-        
-        if (FBTrace.DBG_PANELS) FBTrace.sysout("wicketsource, onObjectSelected ", object);
-        panel.eclipseResult = "";
-        if (object.hasAttribute("wicketsource")) {
-            panel.selectedWicketSource = object.getAttribute("wicketsource");
-            panel.selectedWicketId = object.getAttribute("wicket:id"); 
-            if (FBTrace.DBG_PANELS) FBTrace.sysout("wicketsource calling displayWicket()");
-            panel.displayWicket();
-        } else {
-        	panel.selectedWicketSource = null;
-        	panel.selectedWicketId = null;
-            if (FBTrace.DBG_PANELS) FBTrace.sysout("wicketsource calling displayWicketEmpty()");
-           	panel.displayWicketEmpty();
-        }
-
+        panel.onObjectSelected(object, panel);
     }
-    
 });
 
 Firebug.registerUIListener(Firebug.WicketSourceModel);
 Firebug.registerPanel(WicketSourcePanel);
 Firebug.registerModule(Firebug.WicketSourceModel);
+}
 
-
-
-
-}});
+});
