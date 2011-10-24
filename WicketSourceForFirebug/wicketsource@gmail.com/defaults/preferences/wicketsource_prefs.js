3a0af724fd9fe87a16bee0d8c92538374ca4d647
Jenny Brown <jennybrown8@gmail.com>
2011-10-24 12:28:41 -0500

    Renamed

---
+++ WicketSourceForFirebug/wicketsource@gmail.com/defaults/preferences/wicketsource_prefs.js
@@ -0,0 +1,5 @@
+pref("extensions.firebug.wicketsource.server", "localhost");
+pref("extensions.firebug.wicketsource.port", 9124);
+pref("extensions.firebug.wicketsource.timeout", 4000);
+pref("extensions.firebug.wicketsource.password", "");
+
