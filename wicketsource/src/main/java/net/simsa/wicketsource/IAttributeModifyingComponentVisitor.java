4644d441dd29fafe269120e916e04e83459c5634
Jenny Brown <jenny@42lines.net>
2011-11-21 13:08:38 -0600

    Repackaging to net.ftlines tree

--- wicketsource/src/main/java/net/simsa/wicketsource/IAttributeModifyingComponentVisitor.java
+++
@@ -1,9 +0,0 @@
-package net.simsa.wicketsource;
-
-import org.apache.wicket.Page;
-
-public interface IAttributeModifyingComponentVisitor {
-
-	public abstract void addClassNameVisitor(Page page);
-
-}
