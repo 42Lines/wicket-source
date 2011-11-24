8960d0e561f9bb04e23fb8ae2598e42d94d26b64
Jenny Brown <jenny@42lines.net>
2011-11-24 01:31:59 -0600

    Progress on reading selected node's attributes.

--- WicketSourceForChrome/devtools.js
+++ WicketSourceForChrome/devtools.js
@@ -1,56 +1,79 @@
 // -------- begin clip from sample extension -------------
 /*
-function Console() {
-}
+ function Console() {
+ }
 
-Console.Type = {
-	LOG : "log",
-	DEBUG : "debug",
-	INFO : "info",
-	WARN : "warn",
-	ERROR : "error",
-	GROUP : "group",
-	GROUP_COLLAPSED : "groupCollapsed",
-	GROUP_END : "groupEnd"
-};
+ Console.Type = {
+ LOG : "log",
+ DEBUG : "debug",
+ INFO : "info",
+ WARN : "warn",
+ ERROR : "error",
+ GROUP : "group",
+ GROUP_COLLAPSED : "groupCollapsed",
+ GROUP_END : "groupEnd"
+ };
 
-Console.addMessage = function(type, format, args) {
-	chrome.extension.sendRequest({
-		command : "sendToConsole",
-		tabId : chrome.experimental.devtools.tabId,
-		args : escape(JSON.stringify(Array.prototype.slice.call(arguments, 0)))
-	});
-};
+ Console.addMessage = function(type, format, args) {
+ chrome.extension.sendRequest({
+ command : "sendToConsole",
+ tabId : chrome.experimental.devtools.tabId,
+ args : escape(JSON.stringify(Array.prototype.slice.call(arguments, 0)))
+ });
+ };
 
-// Generate Console output methods, i.e. Console.log(), Console.debug() etc.
-(function() {
-	var console_types = Object.getOwnPropertyNames(Console.Type);
-	for ( var type = 0; type < console_types.length; ++type) {
-		var method_name = Console.Type[console_types[type]];
-		Console[method_name] = Console.addMessage.bind(Console, method_name);
-	}
-})();
-// -------- end -------------
-
-function WicketSource() {
-
-};
-*/
-
-
-
+ // Generate Console output methods, i.e. Console.log(), Console.debug() etc.
+ (function() {
+ var console_types = Object.getOwnPropertyNames(Console.Type);
+ for ( var type = 0; type < console_types.length; ++type) {
+ var method_name = Console.Type[console_types[type]];
+ Console[method_name] = Console.addMessage.bind(Console, method_name);
+ }
+ })();
+ // -------- end -------------
+ */
 
 // The function below is executed in the context of the inspected
 // page because it is called from sidebar.setExpression.
-
 var page_getProperties = function() {
-	
+
 	// Data container.
 	function WicketProperties() {
-		this.wicketId = "a";
-		this.packageName = "b";
-		this.className = "c";
+		this.wicketId = null;
+		this.packageName = null;
+		this.sourceLine = null;
+		this.debug = null;
+		this.eclipseResult = null;
 	}
+	;
+	function parseNode() {
+		var wp = new WicketProperties();
+		var sourceFile = "";
+		var lineNumber = "";
+		var pieces = $0.attributes.wicketsource.value.split(":");
+		if (pieces.length == 3) {
+			wp.packageName = pieces[0];
+			sourceFile = pieces[1];
+			lineNumber = pieces[2];
+		} else if (pieces.length == 2) {
+			sourceFile = pieces[0];
+			lineNumber = pieces[1];
+		}
+		wp.sourceLine = sourceFile + ":" + lineNumber;
+		if (wp.packageName == null) {
+			wp.packageName = "";
+		}
+		wp.wicketId = $0.attributes['wicket:id'].value;
+		return wp;
+	}
+	;
+	function makeUrl() {
+		var url = "http://" + "localhost" + ":" + 9123 + "/open?src="
+				+ encodeURIComponent(this.selectedWicketSource) + "&p="
+				+ encodeURIComponent("");
+		return url;
+	}
+	;
 	function shallowCopy(data) {
 		// Make a shallow copy with a null prototype, so that sidebar does not
 		// expose prototype.
@@ -61,21 +84,33 @@ var page_getProperties = function() {
 		for ( var i = 0; i < props.length; ++i)
 			copy[props[i]] = data[props[i]];
 		return copy;
-	};
-	
-	var wp = new WicketProperties();
-	wp.className = shallowCopy( window.jQuery && $0 ? jQuery.data($0) : {});
+	}
+	;
 
+	var wp = parseNode();
 	return shallowCopy(wp);
 };
 
 chrome.experimental.devtools.panels.elements.createSidebarPane("WicketSource",
 		function(sidebar) {
-
-			function updateElementProperties(stuff) {
-				sidebar.setExpression("(" + page_getProperties.toString() + ")()", "WicketProperties");
+			var addButton = document.createElement("button");
+			addButton.className = "pane-title-button goeclipse";
+			addButton.id = "go-eclipse-button-id";
+//			addButton.title = WebInspector.UIString("View in Eclipse");
+////			addButton.addEventListener("click", _goEclipse.bind(this),false);
+//			this.titleElement.appendChild(addButton);
+			
+			
+			function updateElementProperties() {
+				sidebar.setExpression("(" + page_getProperties.toString()
+						+ ")()", "Component Properties");
 			}
-			updateElementProperties();
 			chrome.experimental.devtools.panels.elements.onSelectionChanged
 					.addListener(updateElementProperties);
+			
+			function _goEclipse(event) {
+		        event.stopPropagation();
+		        this.expanded = true;
+			}
+			this.prototype.__proto__ = WebInspector.SidebarPane.prototype;
 		});
