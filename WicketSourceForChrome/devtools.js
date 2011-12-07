49a7eaf509d9681bfe63eed626380e939fb29d6f
Jenny Brown <jenny@42lines.net>
2011-12-07 03:14:02 -0600

    Major rewrite. Tip on using inspectedWindow.eval enabled data handling, resulting in a much simpler implementation and no content script and no jsonp risks. Simple xmlHttpRequest on an html page layout.

--- WicketSourceForChrome/devtools.js
+++ WicketSourceForChrome/devtools.js
@@ -1,128 +1,11 @@
 chrome.experimental.devtools.panels.elements.createSidebarPane("WicketSource", function(sidebar) {
 
-	// The function below is executed in the context of the inspected
-	// page because it is called from sidebar.setExpression. Because it is
-	// in a different context, functions and variables used must be accessible from there.
-	function page_getProperties(server, port, password) {
-		var hiddenDivId = "wicket-source-chrome-div";
-		var hiddenNodeId = "wicket-source-chrome-data";
+	this.wicketsourceString = null;
+	this.wicketIdString = null;
 		
-		function Message() {
-			this.message = "Select a node with a wicketsource attribute to see details.";
-		}
-
-		// Data container.
-		function WicketProperties() {
-			this.wicketId = null;
-			this.packageName = null;
-			this.sourceLine = null;
-			this.eclipseUrl = null;
-		}
-		function shallowCopy(data) {
-			// Make a shallow copy with a null prototype, so that sidebar does not
-			// expose prototype.
-			var props = Object.getOwnPropertyNames(data);
-			var copy = {
-				__proto__ : null
-			};
-			for ( var i = 0; i < props.length; ++i)
-				copy[props[i]] = data[props[i]];
-			return copy;
-		}
-		
-		// Parses the wicketsource attribute into a json object.
-		function parseNode() {
-			var wp = new WicketProperties();
-			var sourceFile = "";
-			var lineNumber = "";
-			if (($0 == null) || ($0.attributes.wicketsource == undefined)) {
-				return wp;
-			}
-			var pieces = $0.attributes.wicketsource.value.split(":");
-			if (pieces.length == 3) {
-				wp.packageName = pieces[0];
-				sourceFile = pieces[1];
-				lineNumber = pieces[2];
-			} else if (pieces.length == 2) {
-				sourceFile = pieces[0];
-				lineNumber = pieces[1];
-			}
-			wp.sourceLine = sourceFile + ":" + lineNumber;
-			if (wp.packageName == null) {
-				wp.packageName = "";
-			}
-			if ($0.attributes['wicket:id']) {
-				wp.wicketId = $0.attributes['wicket:id'].value;
-			} else {
-				wp.wicketId = null;
-			}
-			
-			wp.eclipseUrl = "http://" + server + ":" + port + "/open?src=" + encodeURIComponent($0.attributes.wicketsource.value) + "&p=" + password;
-
-			// Find the hidden data div we're using. The content script should have already created it.
-			var wicketsourceDiv = document.getElementById(hiddenDivId);
-			
-			// But if our content scripts were disabled, say so.
-			if (!wicketsourceDiv) { 
-				wp.wmessage="Unfamiliar website; click-through disabled for security.";
-			}			
-			
-			return wp;
-		}
-		
-		function updateDataDiv(wp)
-		{
-			// Find the hidden data div we're using. The content script should have already created it.
-			var wicketsourceDiv = document.getElementById(hiddenDivId);
-			
-			// If our content scripts are disabled, just bail.
-			if (!wicketsourceDiv) { 
-				return;
-			}
-			
-			// Empty out any children from previous uses.
-			blankDataDiv();
-
-			// Create the new data contents based on currently selected wicketsource node.
-			var nodeA = document.createElement("a");
-			nodeA.setAttribute("id", hiddenNodeId);
-			nodeA.setAttribute("href", "javascript:ajaxFetch('" + wp.eclipseUrl+"');");
-			nodeA.setAttribute("data", wp.packageName + ":" + wp.sourceLine);
-			nodeA.setAttribute("target", "_wicketsource_chrome_eclipse");
-			nodeA.appendChild(document.createTextNode(wp.sourceLine));
-			wicketsourceDiv.appendChild(nodeA);
-		}
-
-		function goEclipse(url) {
-			var xmlhttp = new XMLHttpRequest();
-			xmlhttp.open("GET", url, true);
-			xmlhttp.send();
-		}
-		
-		// When not in use, delete the div entirely so it doesn't show.
-		function blankDataDiv()
-		{
-			var wicketsourceDiv = document.getElementById(hiddenDivId);
-			if (wicketsourceDiv && wicketsourceDiv.childNodes.length > 0) {
-				var dataNode = document.getElementById(hiddenNodeId);
-				if (dataNode) {
-					wicketsourceDiv.removeChild(document.getElementById(hiddenNodeId));
-				}
-			}
-		}
-
-		// Gets the currently selected node, and if it's not a wicketsource node, shows a default message.
-		if (($0 == null) || ($0.attributes == null) || ($0.attributes.wicketsource == null)) {
-			blankDataDiv();
-			return shallowCopy(new Message());
-		}
-
-		// If it is a wicketsource node, parse it and return the json for display.
-		var wp = parseNode();
-		updateDataDiv(wp);
-		return shallowCopy(wp);
+	function Message() {
+		this.message = "Select a node with a wicketsource attribute to see details.";
 	}
-
 	
 	function getServer() {
 		if (localStorage["server"]) {
@@ -143,12 +26,44 @@ chrome.experimental.devtools.panels.elements.createSidebarPane("WicketSource", f
 		return "";
 	}	
 	
-	
 	// Listener event callback initiates "inspected-page context" data retrieval 
 	// (which understands currently selected node in the Inspect Element tab). 
 	function update() {
-		sidebar.setExpression("(" + page_getProperties.toString() + ")('" + getServer() + "','" + getPort() + "','" + getPassword() + "')", "Component Properties");
+		chrome.experimental.devtools.inspectedWindow.eval("$0.attributes['wicket:id'].value", wicketIdEval);
+		chrome.experimental.devtools.inspectedWindow.eval("$0.attributes.wicketsource.value", wicketsourceEval);
 	}
+	function wicketIdEval(result, isException)
+	{
+		if (!isException) { 
+			this.wicketIdString = result;
+		} else {
+			this.wicketIdString = null;
+		}
+	}
+	function wicketsourceEval(result, isException)
+	{
+		if (!isException) { 
+			this.wicketsourceString = result;
+		} else {
+			this.wicketsourceString = null;
+		}
+		displaySidebar();
+	}
+	function displaySidebar() 
+	{
+		if (this.wicketsourceString == null) {
+			var msg = new Message();
+			sidebar.setObject(msg);
+			return;
+		}
+		sidebar.setPage("sidebar.html?ws=" + encodeURIComponent(this.wicketsourceString) 
+				+ "&wid=" + encodeURIComponent(this.wicketIdString)
+				+ "&server=" + encodeURIComponent(getServer())
+				+ "&port=" + encodeURIComponent(getPort())
+				+ "&p=" + encodeURIComponent(getPassword())
+		);
+	}
+	
 	
 	// On the way in the first time, show an instructional message.
 	update();
---
+++ WicketSourceForChrome/sidebar.js
@@ -0,0 +1,123 @@
+
+
+// Data container.
+function WicketProperties() {
+	this.wicketId = null;
+	this.packageName = null;
+	this.sourceLine = null;
+	this.eclipseUrl = null;
+}
+
+
+// Parses the wicketsource attribute into a json object.
+function parseNode(wicketsourceString, wicketIdString, server, port, password) {
+	var wp = new WicketProperties();
+	var sourceFile = "";
+	var lineNumber = "";
+	if (wicketsourceString == undefined) {
+		return wp;
+	}
+	var pieces = wicketsourceString.split(":");
+	if (pieces.length == 3) {
+		wp.packageName = pieces[0];
+		sourceFile = pieces[1];
+		lineNumber = pieces[2];
+	} else if (pieces.length == 2) {
+		sourceFile = pieces[0];
+		lineNumber = pieces[1];
+	}
+	wp.sourceLine = sourceFile + ":" + lineNumber;
+	if (wp.packageName == null) {
+		wp.packageName = "";
+	}
+	if (wicketIdString) {
+		wp.wicketId = wicketIdString;
+	} else {
+		wp.wicketId = null;
+	}
+	wp.eclipseUrl = "http://" + server + ":" + port + "/open?src=" + encodeURIComponent(wicketsourceString) + "&p=" + password;
+	return wp;
+}
+
+
+function getUrlVars()
+{
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for(var i = 0; i < hashes.length; i++)
+    {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+    }
+    return vars;
+}
+
+function drawDataRow(table, title, value)
+{
+	var tr1 = document.createElement("tr");
+	table.appendChild(tr1);
+	var tdL = document.createElement("td");
+	tdL.setAttribute("class", "dataTitle");
+	tdL.appendChild(document.createTextNode(title));
+	tr1.appendChild(tdL);
+	var tdR = document.createElement("td");
+	tdR.appendChild(document.createTextNode(value));
+	tdR.setAttribute("class", "dataValue");
+	tr1.appendChild(tdR);
+}
+
+function drawLinkRow(table, title, value, wp)
+{
+	var hiddenNodeId = "wicket-source-chrome-data";
+	
+	var nodeA = document.createElement("a");
+	nodeA.setAttribute("id", hiddenNodeId);
+	nodeA.setAttribute("href", "javascript:xmlFetch('" + wp.eclipseUrl+"');");
+	nodeA.setAttribute("data", wp.packageName + ":" + wp.sourceLine);
+	nodeA.appendChild(document.createTextNode(value));
+
+	var tr1 = document.createElement("tr");
+	table.appendChild(tr1);
+	var tdL = document.createElement("td");
+	tdL.setAttribute("class", "dataTitle");
+	tdL.appendChild(document.createTextNode(title));
+	tr1.appendChild(tdL);
+	var tdR = document.createElement("td");
+	tdR.appendChild(nodeA);
+	tdR.setAttribute("class", "dataValue");
+	tr1.appendChild(tdR);	
+}
+
+function xmlFetch(url) {
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.open("GET", url, true);
+	xmlhttp.send();
+}
+
+function drawTable() {
+
+	// All the data we need was presented in the url.
+	var urlVars = getUrlVars();
+	var ws = decodeURIComponent(urlVars["ws"]);
+	var wid = decodeURIComponent(urlVars["wid"]);
+	var server = decodeURIComponent(urlVars["server"]);
+	var port = decodeURIComponent(urlVars["port"]);
+	var password = decodeURIComponent(urlVars["p"]);
+	
+	var wp = parseNode(ws, wid, server, port, password);
+	
+	// Now that we've parsed it, display an html document with a clickable link.
+	var existingTable = document.getElementById("wsTable");
+	if (existingTable) { 
+		document.getElementById("wsDiv").removeChild(existingTable);
+	}
+	
+	var table = document.createElement("table");
+	table.setAttribute("id", "wsTable");
+	drawDataRow(table, "wicket:id", wid);
+	drawDataRow(table, "package", wp.packageName);
+	drawLinkRow(table, "source", wp.sourceLine, wp);
+	
+	document.getElementById("wsDiv").appendChild(table);
+}
